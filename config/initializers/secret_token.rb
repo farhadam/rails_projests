99f84ef993fe488b5a46dc8769ab1db9a689e32b
Farhad <farhad.mibody@gmail.com>
2012-05-21 15:06:37 -0400

    Add mysql2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b9083588133c33bc95e0b1305c9e241d1be04f3ed4bacc9375d613d71fa08063190f02c83db9939155dcee07f518b2e177a9098ca4a2ebe166250b3891ad15fa'
